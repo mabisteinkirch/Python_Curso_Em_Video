6aa2fcbbe9cb07c8c98e03057b1cf9c169b0eb43
mabisteinkirch <70056552+mabisteinkirch@users.noreply.github.com>
2022-05-03 18:39:54 -0300

    input and print example

--- Mundo_01/input_python.py
+++ Mundo_01/input_python.py
@@ -1,3 +1,8 @@
-nome = input ('Qual é seu nome?')
-nome = input ('qual é a sua idade?')
-nome = input ('Qual é o seu peso')
+nome = input ('Qual é seu nome?\n')
+idade = input ('qual é a sua idade?\n')
+peso = input ('Qual é o seu peso?\n')
+
+print('\nA resposta para as variáveis foram:\n'
+      f'Nome: {nome} \n'
+      f'Idade: {idade} \n'
+      f'Peso: {peso} \n')
