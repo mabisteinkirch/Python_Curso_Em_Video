44032230a01745409926e7f7cd6c1b69f0dcb040
mabisteinkirch <70056552+mabisteinkirch@users.noreply.github.com>
2022-05-06 09:03:12 -0300

    ex004 - Mundo 01 - print tipos primitivos

---
+++ Mundo_01/Exercicios/ex004.py
@@ -0,0 +1,16 @@
+#tipos booleanos e testes
+
+a = int(input('Digite um numero: '))
+print (type(a)
+
+b = int(input('Digite uma string: '))
+print (type(b)
+
+c = float(input('Digite um numero flutuante: '))
+print (type(c))
+
+d = int(input('Digite um boolean: '))
+print (type(d))
+#true: se preencheu algo fica como verdadeiro, se não é falso
+
+print ('Resultado {}, {}, {}, {}'.format(a, b, c, d, e))
