beea1999e1c3d526bae8032a7bba230787b0964c
mabisteinkirch <70056552+mabisteinkirch@users.noreply.github.com>
2022-05-06 08:17:46 -0300

    desafio003 - Mundo 01 - diferença com tipoprimitivo

---
+++ Mundo_01/Desafios/desafio_03_2.py
@@ -0,0 +1,17 @@
+#Crie um script Python que leia dois números e "mostre a soma entre eles" com as ferramentas aprendidas ate agora
+
+n1 = input ('Primeiro número:')
+n2 = input ('Segundo número:')
+
+soma = n1+n2
+
+#assim só concatena
+print('Somando sem forçar o tipo primitivo int na leitura, a soma fica como:', soma)
+
+#assim calcula
+
+n1 = int(input ('Primeiro número:'))
+n2 = int(input ('Segundo número:'))
+
+soma = n1+n2
+print('A soma dos dois números são', soma)
