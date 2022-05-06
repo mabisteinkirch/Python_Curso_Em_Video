7cfa4888d16497e946a9614a98ab305ee8107c23
mabisteinkirch <70056552+mabisteinkirch@users.noreply.github.com>
2022-05-06 08:56:50 -0300

    ex003 - Mundo 01 - formatação entre tipos

---
+++ Mundo_01/Exercicios/ex003.py
@@ -0,0 +1,20 @@
+n1 = input ('Digite um valor 1: ')
+
+#ver tipo primitivo da variável
+print (type(n1))
+# str
+
+n2 = int(input('Digite outro valor 2: '))
+#ver tipo primitivo da variável
+print (type(n2))
+
+n3 = int(input('Digite outro valor 3: '))
+
+#erro
+#soman1_n2 = n1 + n2
+
+soman2_n3 = n2 + n3
+
+#erro
+#print ('A soma do n1 com n2 é {}'.format(soman1_n2))
+print ('A soma entre {} e {} é {}'.format(n2, n3, soman2_n3))
